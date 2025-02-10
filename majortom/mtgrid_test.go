69535e63e94fe5d7dd6dc2e9892eaaed32ddaf10
tingold <hutch@earthrise.media>
2025-02-10 18:11:51 -0500

    fixing unit tests

--- majortom/mtgrid_test.go
+++ majortom/mtgrid_test.go
@@ -311,11 +311,11 @@ func TestSmallGrid(t *testing.T) {
 
 func TestOddTile(t *testing.T) {
 	g := NewGrid(320, true)
-	cell, err := g.CellFromId("qr330j8p802")
+	cell, err := g.CellFromId("gcp0ywcrhk1")
 	if err != nil {
 		t.Fail()
 	} else {
-		t.Logf("Expected: qr330j8p802, Got: %s", cell.Id())
+		t.Logf("Expected: gcp0ywcrhk1, Got: %s", cell.Id())
 	}
 	cell, err = g.CellFromId("gcp0ywcrhk1t24vzxu52")
 	if err != nil {
